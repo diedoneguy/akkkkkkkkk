ebe63b20c3719984f84c57a7df28875b98c61563
diedoneguy <akatanaktan@example.com>
2022-05-05 20:46:58 +0600

    first commit

--- azat/settings.py
+++ azat/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from datetime import timedelta
+import  dj_database_url 
+import django_heroku 
 
 
-timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-llet#ns_c2+n8*!e-esw2u=x%b%r#s&b809bekdvh&cf*(+z_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -141,3 +142,8 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
 }
+DATABASES['default'].update(
+   dj_database_url.config(conn_max_age=0, ssl_require=True) 
+)
+
+django_heroku.settings(locals())
